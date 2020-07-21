3455d5fb8074c10f103eb40ffacd3a7c20fa00c4
rajdeep-das <rajdeepdas.india@gmail.com>
2020-07-21 12:11:50 +0530

    Route Varibale

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 # Create app instance 
 app = Flask(__name__)
@@ -6,4 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_workshop():
-    return "Welcome to Web2biz. Team"
+    return "<h1>Welcome to Web2biz. Team<h1>"
+
+
+@app.route('/hello')
+def say_hello():
+    return "hello from hello route"
+
+# route path varibale
+
+@app.route('/hello/<username>')
+def say_hello_to_user(username):
+    return 'Hello %s' % escape(username)
+
