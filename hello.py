4e65ff33d5d02065edfda3603aa787e6fa8d459e
rajdeep-das <rajdeepdas.india@gmail.com>
2020-07-21 12:38:29 +0530

    Templates

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from markupsafe import escape
+from flask import render_template
 
 # Create app instance 
 app = Flask(__name__)
@@ -18,5 +19,17 @@ def say_hello():
 
 @app.route('/hello/<username>')
 def say_hello_to_user(username):
-    return 'Hello %s' % escape(username)
+    return 'Hello %s' %(username)
 
+@app.route('/hello/<int:id>')
+def say_hello_to_id(id):
+    return 'Your Id %d' % (id+1)
+
+# make a simple website
+@app.route('/projects')
+def projects(name=None):
+  return render_template('projects.html',name=name)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
